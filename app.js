026fa7047b801b15c2595a1f96fc59f52280d57a
TIllAd <tilltamino@gmx.de>
2024-01-25 10:51:51 +0100

    add tabs as a test

--- app.js
+++ app.js
@@ -27,3 +27,12 @@ function euclid() {
 
     alert("The GCD is: " + num1);
 }
+
+function showTab(tabId) {
+    var tabs = document.getElementsByClassName('tab-content');
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = 'none';
+    }
+
+    document.getElementById(tabId).style.display = 'block';
+  }
