ae90e56bbf31f7af2bed48cf0492aaada688ac8b
TIllAd <tilltamino@gmx.de>
2024-01-25 11:48:03 +0100

    contruct overhaul

--- app.js
+++ app.js
@@ -15,3 +15,47 @@ function showPanel(panelIndex,colorCode) {
     tabPanels[panelIndex].style.backgroundColor=colorCode;
 }
 showPanel(0,'#f44336');
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function euclid() {
+   
+    var num1 = parseInt(prompt("Enter the first number:"));
+    var num2 = parseInt(prompt("Enter the second number:"));
+    
+    if (isNaN(num1)) {
+        alert("Invalid input for the first number. Please enter a valid number.");
+        return;
+    }
+    
+    if (isNaN(num2)) {
+        alert("Invalid input for the first number. Please enter a valid number.");
+        return;
+    }
+
+    var num2 = parseInt(prompt("Enter the second number:"));
+
+   
+
+    while (num2 !== 0) {
+        var temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+    }
+
+    alert("The GCD is: " + num1);
+}
